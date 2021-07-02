1b44121c5d35320f56b8f88853c0892cada1942d
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2021-07-02 16:52:23 +0200

    set-up react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Listings } from "./sections";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
 
 function App() {
   return (
-    <div className="App">
-      <Listings title="Book it listings" />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
