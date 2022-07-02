2f679921571ed1e1a7aebee696145a6072597887
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2022-07-02 16:08:23 +0200

    add link to home page in not found page

--- src/sections/NotFound/index.tsx
+++ src/sections/NotFound/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./styles/NotFound.css";
 
 export const NotFound = () => {
   return (
     <div className="not-found__container">
-      <h2 className="not-found__text">Page not found</h2>
+      <div className="not-found__message">
+        <h2 className="not-found__text">Page not found</h2>
+        <span className="not-found__home-page-link">
+          <Link to="/">Go to home page</Link>
+        </span>
+      </div>
     </div>
   );
 };
